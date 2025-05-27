919e7fc067c0293bf4646b494a3951372358b6f6
bacemkarray <bkarray1@gmail.com>
2025-05-27 15:46:22 +0400

    feat: implemented exponential smoothing

--- vision/face_tracking.py
+++ vision/face_tracking.py
@@ -3,18 +3,25 @@ import numpy as np
 import cv2 as cv
 import time
 from visualization_utils import visualize
+import serial
+
+# Configure serial communication
+s = serial.Serial(port='COM3',
+                  baudrate=9600)
 
 # Create a FaceDetector object.
 BaseOptions = mp.tasks.BaseOptions
 FaceDetector = mp.tasks.vision.FaceDetector
 FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
-
 options = FaceDetectorOptions(
     base_options=BaseOptions(model_asset_path='vision/blaze_face_short_range.tflite'),
     running_mode=VisionRunningMode.VIDEO)
-
 detector = FaceDetector.create_from_options(options)
+
+# Default servo angles are set to 90 degrees.
+prevX = 90
+prevY = 90
 
 cap = cv.VideoCapture(0)
 ws = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
@@ -23,7 +30,7 @@ hs = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
-    
+
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -44,9 +51,20 @@ while True:
     cv.imshow('Camera', annotated_image)
 
     if face_pos:
+
         # Takes the face_pos coordinates, and assigns a servo angle based on their position relative to the image frame
-        servoX = np.interp(face_pos[0], [0, ws], [0, 180])
-        servoY = np.interp(face_pos[1], [0, hs], [0, 180])
+        servoX = 180-np.interp(face_pos[0], [0, ws], [0, 180])
+        servoY = 180-np.interp(face_pos[1], [0, hs], [0, 180])
+
+        # Simple exponential smoothing
+        alpha = 0.2
+        smoothedX = int(alpha * servoX + (1 - alpha) * prevX)
+        smoothedY = int(alpha * servoY + (1 - alpha) * prevY)
+        prevX = smoothedX
+        prevY = smoothedY
+        
+        # send data to arduino
+        s.write([smoothedX,smoothedY])
 
     # if face_pos[0] is greater than center of screen, move servo-1 at an angle that reduces this value. while face_pos greater than center, 
     # find out window height and width
