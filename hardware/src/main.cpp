f91d4c3bd7bf681c3a367421fc5c64d4f45c2549
bacemkarray <bkarray1@gmail.com>
2025-06-02 11:57:14 +0400

    test: 4 byte serial communication

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,24 +1,40 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-Servo servoX;
-Servo servoY;
+Servo servo_x;
+Servo servo_elbow;
+Servo servo_arm;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);     // Match this with Python serial baudrate
-  servoX.attach(9);       // Set your actual servo pin
-  servoY.attach(10);      // Set your actual servo pin
-  servoX.write(90);
-  servoY.write(90);
+  Serial.begin(115200);
+  servo_x.attach(9);       
+  servo_x.write(90);
+  
+  servo_arm.attach(11);
+  servo_arm.write(0);
+
+  servo_elbow.attach(10);
+  servo_elbow.write(60);
 }
 
 void loop() {
-  if (Serial.available() >= 2) {
-    int angleX = Serial.read();
-    int angleY = Serial.read();
-
-    servoX.write(angleX);
-    servoY.write(angleY);
+  if (Serial.available() >= 4) { // Wait until a 4-byte packet is available
+    uint16_t angleX = Serial.read(); // Read LSB of angleX
+    angleX |= Serial.read() << 8; // Read MSB of angleX
+    uint16_t angleY = Serial.read();
+    angleY |= Serial.read() << 8;
+    
+    servo_x.write(angleX);
+    // servo_elbow.write(angleY);
+    servo_arm.write(angleY);
   }
+  // if (Serial.available() >= 2) {
+  //   int angleX = Serial.read();
+  //   int angleY = Serial.read();
+    
+  //   servo_x.write(angleX);
+  //   // servo_elbow.write(angleY);
+  //   servo_arm.write(angleY);
+    
+  // }
 }
