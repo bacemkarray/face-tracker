023109a8c3e069914a640f60d74e7a06027f7f72
bacemkarray <bkarray1@gmail.com>
2025-07-02 14:15:37 +0400

    Reorganized State Schema

--- vision/agent/graph.py
+++ vision/agent/graph.py
@@ -1,23 +1,12 @@
 from langchain_core.messages import SystemMessage
 from langchain_openai import ChatOpenAI
-from pydantic import BaseModel, Field
 from typing import List, Literal, Optional
 from typing_extensions import TypedDict
-
-from langgraph.graph import START, StateGraph, MessagesState
-
-# -------- SCHEMA -------- #
-class Task(TypedDict):
-    task: Literal["search", "track"]
-    duration: float = None
-    target: str = None
-
-class TaskPlannerState(TypedDict):
-    instruction: str
-    tasks: Optional[List[Task]]  # Will hold the parsed list of tasks
+from langgraph.graph import StateGraph, START, END
 
 
 llm = ChatOpenAI(model="gpt-4o").with_structured_output(method="json_mode")
+
 
 prompt_template = """
 You are a task planner for a robotic arm. Convert the following instruction into a list of task objects in JSON format.
@@ -41,23 +30,41 @@ Output: [
   {{"task": "track", "target": "dad", "duration": 10}}
 ]
 
-Input: {instruction}
+Input: {instructions}
 Output:
 """
 
 
-def generate_task(state : TaskPlannerState):
-    instruction = state["instruction"]
-    prompt = prompt_template.format(instruction=instruction)
+# -------- SCHEMA -------- #
+class Task(TypedDict):
+  task: Literal["search", "track"] # Only temporarily strict. Eventually, agent will decide what mode it should execute.
+  duration: float = None
+  target: str = None
+  
+class InputState(TypedDict):
+  instructions: str
+
+class OutputState(TypedDict):
+  tasks: Optional[List[Task]]
+
+class OverallState(InputState, OutputState):
+  pass
+
+
+# -------- NODES -------- #
+def generate_task(state : InputState) -> OutputState:
+    instructions = state["instructions"]
+    prompt = prompt_template.format(instructions=instructions)
     system_message = SystemMessage(content=prompt)
     response = llm.invoke([system_message])
     return {"tasks": response}
 
 
-# Build and compile graph
+# -------- GRAPH -------- #
 graph = (
-    StateGraph(TaskPlannerState)
-    .add_node("generate_tasks", generate_task)
-    .add_edge(START, "generate_tasks")
+    StateGraph(OverallState, input_schema=InputState, output_schema=OutputState)
+    .add_node("create_task", generate_task)
+    .add_edge(START, "create_task")
+    .add_edge("create_task", END)
     .compile()
 )
