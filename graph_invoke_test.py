55bd3541e51f0a5561965e9c517e6ab593046071
bacemkarray <bkarray1@gmail.com>
2025-07-30 16:11:06 +0100

    added print debugging statements

--- graph_invoke_test.py
+++ graph_invoke_test.py
@@ -2,10 +2,12 @@ from langgraph_sdk import get_client
 import asyncio
 
 async def send_embedding(face_id: int, emb: list[float]):
+    print(f"🧠 send_embedding CALLED for ID {face_id}")
     client = get_client(url="http://localhost:8123")
     await client.store.put_item(namespace=["face_embeddings"],
                                 key=face_id,
                                 value={"embedding": emb})
+    print(f"✅ embedding STORED for ID {face_id}")
 
 
 
@@ -19,8 +21,7 @@ async def main():
     #                             value={"embedding": [0.1, 0.2, 0.3, 0.4]})
     
     namespaces = await client.store.list_namespaces(prefix=["face_embeddings"])
-    item = await client.store.get_item(["face_embeddings"],
-                                       key="bacem")
+    item = await client.store.search_items(["face_embeddings"])
 
     print(namespaces)
     print(item)
